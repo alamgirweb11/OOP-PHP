5c83c474444786499dadb6231488a4b82e04798f
alamgirweb11 <alamgirweb11@gmail.com>
2020-06-27 22:18:32 +0600

    initial upload

---
+++ Inheritance/child_class_creation.php
@@ -0,0 +1,11 @@
+<?php 
+// parent calss
+class ParentClass{
+	    // property and method declare
+}
+// child class
+
+class ClassName extends AnotherClass
+{
+       // write additional properties and methods
+}
